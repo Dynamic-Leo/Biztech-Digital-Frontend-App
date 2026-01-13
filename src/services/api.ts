201056826c1cd73270986385b802ae78cef780ac
Ehtisham Naveed <75159969+ehtishamnaveed@users.noreply.github.com>
2026-01-13 16:11:32 +0500

    updated production url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 type ApiError = Error & { status?: number; data?: any };
 
